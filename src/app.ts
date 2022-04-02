7ce793ccb3a89ca4c437ca8b4de3956cb1a2d5ea
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2022-04-02 13:20:09 -0700

    barebone server

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application } from "express";
+
+
+export class App {
+    private app: Application;
+
+    constructor(private port? : number | string) {
+        this.app = express();
+        this.middlewares();
+    }
+
+    settings() {
+        this.app.set("port", process.env.PORT || 3000)
+    }
+
+    middlewares() {
+
+    }
+
+    async listen() {
+        this.app.listen(this.port);
+        console.log("Listening on port: ",this.port);
+        
+    }
+}
