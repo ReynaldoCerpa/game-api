0b866a7bdfaec7ea45c3a37c7f4dbb025138227a
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2023-02-09 18:42:25 -0800

    first commit

--- src/sockets.ts
+++ src/sockets.ts
@@ -1,9 +1,25 @@
 import { Socket } from "socket.io";
 
 export default (io: any) => {
+	const socketCounter = 0
+
     io.on("connection", (socket: Socket) => {
+	  socketCounter++
       // console.log(socket.handshake.url);
       console.log("nuevo socket connectado:", socket.id);
+
+	  if (socketCounter == 2){
+		io.emit("server:startcounter")
+	  }
+
+	  socket.on("client:counterend", () => {
+		for (let i = 0; i < 3; i++) {
+		  setTimeout(() => {
+			let randnum = Math.floor(Math.random() * (19 - 11) + 11)
+			io.emit("server:newnumber",randnum)
+		  }, 5000)
+		}
+	  })
 
       socket.on("client:message", (msg) => {
           console.log(msg);
